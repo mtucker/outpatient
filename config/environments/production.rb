c1d3bd2383ea7865552cf0ed8ad872156d065f65
Matt Tucker <matt.d.tucker@gmail.com>
2014-08-28 18:43:48 -0400

    Configure default mailer URLs for dev and prod.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Default mailer URL
+  # TODO: this probably needs to change when we get a domain
+  config.action_mailer.default_url_options = { :host => 'chatmd.heroku.com' }
 end
