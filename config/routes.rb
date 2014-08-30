b9cd7c9050300ed546290d2accc50945f88ba92c
Matt Tucker <matt.d.tucker@gmail.com>
2014-08-30 15:12:15 -0400

    Show login as default page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
