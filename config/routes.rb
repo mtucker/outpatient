a1d0555c8132c3fb135fd619fe6eaf1f6e4ed4c0
Matt Tucker <matt.d.tucker@gmail.com>
2014-08-30 17:23:52 -0400

    Generate scaffold for Providers and add devise paths.
    Probably will want to trim some of this back later.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  devise_for :providers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+
+  resources :providers
+
+  root 'welcome#index'
 
   # You can have the root of your site routed with "root"
 
