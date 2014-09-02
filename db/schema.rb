6df16c09eeb0784d9aa3b0d8a92080a2df126a5c
Matt Tucker <matt.d.tucker@gmail.com>
2014-09-01 21:18:08 -0400

    Schema update adding name, phone, zip to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828230533) do
+ActiveRecord::Schema.define(version: 20140831230252) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",     null: false
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 20140828230533) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "phone"
+    t.string   "zip_code"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
