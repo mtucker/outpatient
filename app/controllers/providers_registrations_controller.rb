149678a8699e8edefedba67ccd45697df36aa354
Matt Tucker <matt.d.tucker@gmail.com>
2014-09-01 21:15:57 -0400

    Create custom registration controller and views for providers

---
+++ app/controllers/providers_registrations_controller.rb
@@ -0,0 +1,11 @@
+class ProvidersRegistrationsController < Devise::RegistrationsController
+
+  before_filter :configure_permitted_parameters
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up).push(:name, :phone, :zip_code)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  devise_for :providers
+
+  devise_for :users,
+              path: "/",
+              path_names: {sign_in: 'login', sign_out: 'logout'}
+  devise_for :providers,
+              controllers: {registrations: 'providers_registrations'},
+              path_names: {sign_up: 'register'}
+
+  resources :providers
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :providers
 
   root 'welcome#index'
 
