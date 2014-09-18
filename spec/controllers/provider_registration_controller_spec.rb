da42231fe265c10b615244b375ba973e3b2c40da
Matt Tucker <matt.d.tucker@gmail.com>
2014-09-18 13:29:22 -0400

    Specs for ProviderRegistrationsController

---
+++ spec/controllers/provider_registration_controller_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe ProvidersRegistrationsController, :type => :controller do
+
+  before do
+    request.env["devise.mapping"] = Devise.mappings[:provider]
+  end
+
+  describe "GET #new" do
+
+    it 'assigns a new Provider to @provider' do
+      get :new
+      expect(assigns(:provider)).to be_a Provider
+    end
+
+    it 'renders the :new template' do
+      get :new
+      expect(response).to render_template :new
+    end
+
+  end
+
+  describe "POST #create" do
+
+    context 'with valid attributes' do
+      it 'creates a new provider record in the database' do
+        expect{post :create, provider: attributes_for(:provider)}.to change { User.count }.by(1)
+      end
+
+      it 'redirects to the provider profile' do
+        post :create, provider: attributes_for(:provider)
+        expect(response).to redirect_to Provider.last
+      end
+
+    end
+
+    context 'with invalid attributes' do
+
+      it 'does not save a new provider record to the database' do
+        expect{post :create, provider: attributes_for(:invalid_provider)}.not_to change { User.count }
+      end
+
+      it 're-renders the :new template' do
+        post :create, provider: attributes_for(:invalid_provider)
+        expect(response).to render_template :new
+      end
+
+    end
+
+  end
+
+end
