841f9db7b99690ee4dc2b9fa2d407aa1b4cf3385
Matt Tucker <matt.d.tucker@gmail.com>
2014-10-08 12:56:52 -0400

    Refactor password creation in Administrator factory.

--- spec/factories/administrators.rb
+++ spec/factories/administrators.rb
@@ -4,10 +4,7 @@ FactoryGirl.define do
   factory :administrator do |f|
     f.name { Faker::Name.name }
     f.email { Faker::Internet.email }
-
-    password = Faker::Internet.password
-    f.password { password }
-    f.password_confirmation { password }
+    f.password { Faker::Internet.password }
+    f.password_confirmation { "#{password}" }
   end
-
 end
