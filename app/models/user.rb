85a045d58aa7c97b7e45c43f65af895e9a3a8479
Matt Tucker <matt.d.tucker@gmail.com>
2014-10-08 17:25:12 -0400

    Add callbacks to User model for activation/deactivation dates.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
 class User < ActiveRecord::Base
 
   acts_as_paranoid
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_update :set_activation_dates, if: :is_active_changed?
+
+  private
+
+  def set_activation_dates
+
+    if(self.is_active)
+      self.activated_at = Time.now
+    else
+      self.deactivated_at = Time.now
+    end
+
+  end
+
 end
