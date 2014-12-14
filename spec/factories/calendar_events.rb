bff7d8a03560c751494fb23b2560a163fe61f705
Matthew Tucker <matt@outpatient.co>
2014-12-14 14:38:24 -0500

    Fix CalendarEvent factory and add Availability factory.

--- spec/factories/calendar_events.rb
+++ spec/factories/calendar_events.rb
@@ -1,10 +1,14 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :calendar_event do
-    user nil
-    type ""
-    starts_at "2014-12-12 14:03:41"
-    ends_at "2014-12-12 14:03:41"
+  factory :calendar_event do |f|
+    f.starts_at DateTime.now
+    f.ends_at 1.hour.from_now 
+    user
+  end
+
+
+  factory :invalid_calendar_event, parent: :calendar_event do |f|
+    f.starts_at nil
   end
 end
