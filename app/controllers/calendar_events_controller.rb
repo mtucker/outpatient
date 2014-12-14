932dd5703c8263289d9c12282b79b6a6b36af6db
Matthew Tucker <matt@outpatient.co>
2014-12-14 14:40:36 -0500

    Add CalendarEventsController w/ new and create functions and associated specs.

--- app/controllers/calendar_events_controller.rb
+++ app/controllers/calendar_events_controller.rb
@@ -1,2 +1,34 @@
-class CalendarEventsController < InheritedResources::Base
+class CalendarEventsController < ApplicationController
+
+  before_filter :authenticate_user!
+
+  def new
+    @calendar_event = CalendarEvent.new
+  end
+
+  def create
+    @calendar_event = CalendarEvent.new(calendar_event_params)
+
+    if !@calendar_event.user
+      @calendar_event.user = current_user
+    end
+
+    respond_to do |format|
+      if @calendar_event.save
+        format.html { redirect_to @calendar_event, notice: 'Calendar event was successfully created.' }
+        format.json { render :show, status: :created, location: @calendar_event }
+      else
+        format.html { render :new }
+        format.json { render json: @calendar_event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def calendar_event_params
+    params.require(:calendar_event).permit(:user_id, :type, :starts_at, :ends_at)
+  end
+
 end
