3efb297e4e1daaba694d92bf63540d5836774b54
Matthew Tucker <matt@outpatient.co>
2015-01-07 16:42:31 -0600

    Correct emails for admin seeds.

--- db/seeds/administrators.seeds.rb
+++ db/seeds/administrators.seeds.rb
@@ -1,12 +1,12 @@
 Administrator.find_or_create_by(email: 'matt@outpatient.co') do |admin|
-  admin.email = 'matt.d.tucker@gmail.com'
+  admin.email = 'matt@outpatient.co'
   admin.name = 'Matt Tucker'
   admin.password = 'admin123'
   admin.password_confirmation = 'admin123'
 end
 
 Administrator.find_or_create_by(email: 'kunal@outpatient.co') do |admin|
-  admin.email = 'kunal.x.mehta@gmail.com'
+  admin.email = 'kunal@outpatient.co'
   admin.name = 'Kunal Mehta'
   admin.password = 'admin123'
   admin.password_confirmation = 'admin123'
