a18dad7e33173c5d63ffc5a95f6c75b54e19fc95
Matthew Tucker <matt@outpatient.co>
2015-01-09 10:15:18 -0600

    Remove unused Availability classes.

--- app/models/availability.rb
+++
@@ -1,7 +0,0 @@
-class Availability < CalendarEvent
-
-  scope :user, -> (user) { where("user_id = ?", "#{user.id}")}
-  scope :starts_after, -> (start_dttm) { where("starts_at >= ?", "#{start_dttm}")}
-  scope :starts_before, -> (end_dttm) { where("starts_at <= ?", "#{end_dttm}")}
-
-end
