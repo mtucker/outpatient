3795b5a4c36421005e3f6f2c9a14a5a1eb5e3ef1
Matthew Tucker <matt@outpatient.co>
2015-01-13 15:30:32 -0600

    Rename FullCalendarHelper to CalendarHelpers
    It now contains methods not exclusively for FullCalendar

--- spec/support/05.calendar_helpers.rb
+++ spec/support/05.calendar_helpers.rb
@@ -1,19 +1,31 @@
-# These are helpers to interact with the FullCalendar
-# JQuery plugin we use to manage Providers' schedules.
-module FullCalendarHelpers
+# These are helpers to interact with the calendar
+# we use to manage Providers' schedules.
+module CalendarHelpers
 
   # Simulates clicking on a FullCalendar datetime
   def day_click(calendar_selector, dttm)
 
     # Convert the date to an ISO 8601 formatted string
-    dttm_string = dttm.strftime('%F')
+    dttm_string = dttm.strftime('%Y-%m-%dT%H:%M:%S%z')
 
     # Trigger FullCalendars 'dayClock' callback to simulate
     # a click on the JS calendar
-    page.evaluate_script(
+    page.execute_script(
       "
         date = moment('#{dttm_string}', moment.ISO_8601);
         $('#{calendar_selector}').fullCalendar('trigger', 'dayClick', null, date, null, null);
+      "
+    )
+
+  end
+
+  def select_date(date_picker_selector, date)
+
+    date_string = date.strftime('%Y/%m/%d')
+
+    page.execute_script(
+      "
+        $('#{date_picker_selector}').val('#{date_string}');
       "
     )
 
