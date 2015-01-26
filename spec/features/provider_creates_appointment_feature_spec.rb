34e1951d493dbaf6d7dd97fe7a089c2bdfc7d589
Matthew Tucker <matt@outpatient.co>
2015-01-26 17:24:01 -0500

    Verify event type select box includes appointment type.

--- spec/features/provider_creates_appointment_feature_spec.rb
+++ spec/features/provider_creates_appointment_feature_spec.rb
@@ -1,18 +1,42 @@
 require 'rails_helper'
 
-feature 'Provider creates a Appointment' do
+feature 'Provider creates an Appointment' do
 
   scenario 'Provider clicks the Calendar and is presented with the option to create an Appointment', js: true do
 
     provider_signs_in
 
-    expect(page).not_to have_text('Schedule availability for virtual appointments.')
-
     click_link 'Calendar'
 
     click_day('#calendar', Time.now)
 
-    expect(page).to have_text('Schedule availability for virtual appointments.')
+    expect(page).to have_select(
+      'calendar_event_calendar_event_type_id', 
+      with_options: [
+        'Schedule availability for virtual appointments.', 
+        'Schedule a virtual appointment.'
+      ]
+    )
+
+  end
+
+  scenario 'An Appointment and Calendar Event are created', js: true do
+
+    provider_signs_in
+
+    click_link 'Calendar'
+
+    now = Time.now.round_down_to_nearest_half_hour
+
+    click_day('#calendar', now)
+
+    select('Schedule a virtual appointment.', from: 'calendar_event_calendar_event_type_id')
+
+    click_button 'Save'
+
+    within('.fc-event'){
+      expect(page).to have_calendar_event('Available', now, now + 1.hour, 1)
+    }
 
   end
 
