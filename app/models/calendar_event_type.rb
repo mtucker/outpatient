ef840969169221dac91af8c3a9ef45554aa91225
Matthew Tucker <matt@outpatient.co>
2015-01-26 17:18:43 -0500

    CalendarEventType label depends on type name.

--- app/models/calendar_event_type.rb
+++ app/models/calendar_event_type.rb
@@ -2,7 +2,7 @@ class CalendarEventType < ActiveRecord::Base
 
   def calendar_event_type_label
 
-    I18n.t 'forms.calendar_events.calendar_event_types.online_appointment_availability.new'
+    I18n.t "forms.calendar_events.calendar_event_types.#{name.downcase}.new"
 
   end
 
