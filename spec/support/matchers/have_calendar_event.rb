290e231d88380b42c681c876321edd9ca630c120
Matthew Tucker <matt@outpatient.co>
2015-02-03 18:54:51 -0500

    Improve have_calendar_event matcher.

--- spec/support/matchers/have_calendar_event.rb
+++ spec/support/matchers/have_calendar_event.rb
@@ -1,4 +1,3 @@
-3
 module Capybara
   module RSpecMatchers
     class HaveCalendarEvent < Matcher
@@ -12,14 +11,14 @@ module Capybara
       end
 
       def matches?(actual)
-        wrap(actual).assert_text("#{@starts_at.strftime('%l:%M')} - #{@ends_at.strftime('%l:%M')} #{@title}", count: @count)
+        wrap(actual).assert_selector("a[data-starts-at='#{@starts_at.iso8601}'][data-ends-at='#{@ends_at.iso8601}']", count: @count)
       rescue Capybara::ExpectationNotMet => e
         @failure_message = e.message
         return false
       end
 
       def does_not_match?(actual)
-        wrap(actual).assert_no_text("#{@starts_at.strftime('%l:%M')} - #{@ends_at.strftime('%l:%M')}")
+        wrap(actual).assert_no_selector("a[data-starts-at='#{@starts_at.iso8601}'][data-ends-at='#{@ends_at.iso8601}']", count: @count)
       rescue Capybara::ExpectationNotMet => e
         @failure_message_when_negated = e.message
         return false
